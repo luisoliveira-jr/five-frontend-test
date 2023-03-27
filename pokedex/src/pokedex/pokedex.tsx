dbb69683833c4824a5cccd740f0fa1efe4af1ce3
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-03-26 22:20:07 -0300

    Controle life cycle e efeitos com a hook useEffect

--- pokedex/src/pokedex/pokedex.tsx
+++ pokedex/src/pokedex/pokedex.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface PokedexProps { }
 
@@ -8,6 +8,17 @@ export const Pokedex: React.FC<PokedexProps> = () => {
     const [pokemons, setPkemons] = useState<string[]>(pokemonsArray);
     const [selectedPokemon, setSelectedpokemon] = useState<string | undefined>('');
 
+    //Com array vazio executa somente quando instanciado
+    useEffect(() => {
+       alert("O componente foi instanciado")
+    }, []);
+
+    //Com variável dentro do array, ele observa e executa a cada atualização da variável.
+    useEffect(() => {
+        if (!selectedPokemon) return;
+        alert(selectedPokemon)
+    }, [selectedPokemon]);
+
 
     return (
         <div>
@@ -16,7 +27,7 @@ export const Pokedex: React.FC<PokedexProps> = () => {
             <h2>Pokemons: </h2>
             {pokemons.map((pokemon) => <button onClick={() => setSelectedpokemon(pokemon)}>{pokemon}</button>)}
             <br></br>
-            <h2>Pokemon selecionado: {selectedPokemon}</h2>
+            <h2>Pokemon selecionado: {selectedPokemon ? selectedPokemon : "Nenhum pokemon selecionado"}</h2>
 
         </div>
     );
