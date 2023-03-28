2e4d73c5673373aad86024a57aa9b319f80db756
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-03-27 22:02:02 -0300

    install @tanstack/react-query-devtools

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider, } from 'react-query';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import Pokedex from './pokedex/pokedex';
 import { AppRoutes } from './routes';
 
+interface AppProps {
+
+}
+
+const queryClient = new QueryClient()
+
 const App: React.FC = () => {
   return (
-    <>
+    <QueryClientProvider client={new QueryClient}>
       <Router>
         <AppRoutes />
       </Router>
-    </>
+    </QueryClientProvider>
   );
 };
 
